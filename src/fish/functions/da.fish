576096898d25f8ff76ae002c9bccf812e3f1dd69
Toshiaki Asai <toshi.alternative@gmail.com>
2018-12-14 19:01:08 +0900

    [fish] fix docker shorthand

--- src/fish/functions/da.fish
+++ src/fish/functions/da.fish
@@ -1,3 +1,3 @@
 function da --description 'alias da docker attach \$(docker-containers | fzf-tmux)'
-    docker attach \$(docker-containers | fzf-tmux);
+    docker attach (docker-containers | fzf-tmux);
 end
