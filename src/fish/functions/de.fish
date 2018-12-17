41821d9c77aa5d72a8fdc5547b28d5ca683dd478
Toshiaki Asai <toshi.alternative@gmail.com>
2018-12-17 16:36:20 +0900

    [fish] なんかすごいことになってた

--- src/fish/functions/de.fish
+++ src/fish/functions/de.fish
@@ -1,3 +1,3 @@
 function de --description 'alias de docker exec -it \$(docker-containers | fzf-tmux)'
-    echo docker exec -it (docker-containers | fzf-tmux) $@;
+    docker exec -it (docker-containers | fzf-tmux) $argv;
 end
