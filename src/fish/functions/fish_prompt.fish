f902b5747af0a4e283fb7f06957ad8390ce7e2be
Toshiaki Asai <toshi.alternative@gmail.com>
2019-12-02 19:55:42 +0900

    [fish] promptをいい感じにした

--- src/fish/functions/fish_prompt.fish
+++ src/fish/functions/fish_prompt.fish
@@ -1,4 +1,4 @@
 function fish_prompt --description 'Write out the prompt'
     history --merge
-    echo -n -s (set_color yellow) $current_docker (set_color normal) "% "
+    echo -n -s (set_color yellow) $current_docker (set_color normal) "> "
 end
