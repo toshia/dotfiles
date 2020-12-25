c5f4f922933e103eed6229ad8794daaa61ea0f37
Toshiaki Asai <toshi.alternative@gmail.com>
2020-12-25 09:33:27 +0900

    fix

--- src/polybar/launch.sh
+++ src/polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 killall -q polybar
-export PRIMARY_MONITOR=$(polybar --list-monitors | awk -F':' '{ print $1 }')
+export PRIMARY_MONITOR=$(polybar --list-monitors | head -n1 | cut -d":" -f1)
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
