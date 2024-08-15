4966dc5f44b568c217c7ab3339a8729f121be0b8
Toshiaki Asai <toshi.alternative@gmail.com>
2024-08-15 14:53:14 +0900

    edit config i3 and polybar

--- src/polybar/launch.sh
+++ src/polybar/launch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
 killall -q polybar
-export PRIMARY_MONITOR=$(polybar --list-monitors | head -n1 | cut -d":" -f1)
+
+if [[ -z "$1" ]]; then
+    export PRIMARY_MONITOR=$(polybar --list-monitors | head -n1 | cut -d":" -f1)
+else
+    export PRIMARY_MONITOR=$1
+fi
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
